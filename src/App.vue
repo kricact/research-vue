63454c5aaba043b093f095209bac2d45a9511e7d
Kristina Pchelintseva <kristina.pchelintseva@jetbrains.com>
2024-05-02 18:56:45 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Counter" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="title">
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <h1>{{ msg }}</h1>
+  </div>
+  <div class="count-info">count is {{ count }}</div>
+  <div class="card">
+    <button type="button" @click="count++">+</button>
+    <button type="button" @click="count--">-</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.title {
+  display: flex;
+}
+</style>
